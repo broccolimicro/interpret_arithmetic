cf0d39defc74119744fdb55f72b9b863a01bc834
Ned Bingham <edward.bingham@broccolimicro.io>
2026-07-09 07:13:59 -0400

    using expression literal support

--- tests/expression.cpp
+++ tests/expression.cpp
@@ -3,26 +3,27 @@
 
 using parse_expression::precedence_set;
 using parse_expression::operation_set;
+using parse_expression::operation;
 
 void setup_expressions() {
 	if (expression::precedence.empty()) {
 		parse_expression::precedence_set result;
-		result.push(parse_expression::operation_set::BINARY);
+		result.push(operation_set::BINARY);
 		result.push_back("", "", "|", "");
 
-		result.push(parse_expression::operation_set::BINARY);
+		result.push(operation_set::BINARY);
 		result.push_back("", "", "&", "");
 
-		result.push(parse_expression::operation_set::BINARY);
+		result.push(operation_set::BINARY);
 		result.push_back("", "", "^", "");
 
-		result.push(parse_expression::operation_set::BINARY);
+		result.push(operation_set::BINARY);
 		result.push_back("", "", "||", "");
 		
-		result.push(parse_expression::operation_set::BINARY);
+		result.push(operation_set::BINARY);
 		result.push_back("", "", "&&", "");
 
-		result.push(parse_expression::operation_set::BINARY);
+		result.push(operation_set::BINARY);
 		result.push_back("", "", "==", "");
 		result.push_back("", "", "!=", "");
 		result.push_back("", "", "<", "");
@@ -30,42 +31,42 @@ void setup_expressions() {
 		result.push_back("", "", "<=", "");
 		result.push_back("", "", ">=", "");
 
-		result.push(parse_expression::operation_set::BINARY);
+		result.push(operation_set::BINARY);
 		result.push_back("", "", "<<", "");
 		result.push_back("", "", ">>", "");
 
-		result.push(parse_expression::operation_set::BINARY);
+		result.push(operation_set::BINARY);
 		result.push_back("", "", "+", "");
 		result.push_back("", "", "-", "");
 
-		result.push(parse_expression::operation_set::BINARY);
+		result.push(operation_set::BINARY);
 		result.push_back("", "", "*", "");
 		result.push_back("", "", "/", "");
 		result.push_back("", "", "%", "");
 
-		result.push(parse_expression::operation_set::UNARY);
+		result.push(operation_set::UNARY);
 		result.push_back("!", "", "", "");
 		result.push_back("~", "", "", "");
 		result.push_back("(bool)", "", "", "");
 		result.push_back("+", "", "", "");
 		result.push_back("-", "", "", "");
 
-		result.push(parse_expression::operation_set::MODIFIER);
+		result.push(operation_set::MODIFIER);
 		result.push_back("", "!", "", "");
 		
-		result.push(parse_expression::operation_set::UNARY);
+		result.push(operation_set::UNARY);
 		result.push_back("#", "", "", "");
 		result.push_back("", "", "", "?");
 
-		result.push(parse_expression::operation_set::MODIFIER);
-		result.push_back("", "'", "", "");
+		result.push(operation_set::MODIFIER);
+		result.push_back("", "'", "", "", operation::LITERAL, operation::LABEL);
 
-		result.push(parse_expression::operation_set::MODIFIER);
-		result.push_back("", "(", ",", ")");
-		result.push_back("", ".", "", "");
+		result.push(operation_set::MODIFIER);
+		result.push_back("", "(", ",", ")", operation::TERM, operation::LITERAL);
+		result.push_back("", ".", "", "", operation::LITERAL, operation::LABEL);
 		result.push_back("", "[", ":", "]");
 		
-		result.push(parse_expression::operation_set::MODIFIER);
+		result.push(operation_set::MODIFIER);
 		result.push_back("", "::", "", "");
 
 		result.push(operation_set::GROUP);
